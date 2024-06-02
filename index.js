fe76d2306fb8f618540caaf6e4f16a15d658fd40
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-06-02 16:42:08 +0600

    feat(mobile): :sparkles: adds support mobile version

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 // @homepageURL  https://github.com/pagyew/youtube-shortsless
 // @supportURL   https://github.com/pagyew/youtube-shortsless
 // @match        https://www.youtube.com/*
+// @match        https://m.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
@@ -16,15 +17,24 @@
   'use strict';
 
   const selectors = [
-      'yt-chip-cloud-chip-renderer:has(yt-formatted-string[title*="shorts" i])',
+      // Menus
+      'ytm-pivot-bar-item-renderer:has(.pivot-shorts)',
       'ytd-mini-guide-entry-renderer:has(a#endpoint[title="shorts" i])',
       'ytd-guide-entry-renderer:has(a#endpoint[title="shorts" i])',
-      'ytd-rich-section-renderer:has(ytd-rich-shelf-renderer)',
-      'ytd-item-section-renderer:has(ytd-reel-shelf-renderer)',
-      'ytd-video-renderer:has(a#thumbnail[href*="shorts" i])',
+
+      // Tabs
+      'yt-chip-cloud-chip-renderer:has(yt-formatted-string[title*="shorts" i])',
       'yt-tab-shape[tab-title="shorts" i]',
-      'ytd-reel-shelf-renderer'
-  ]
+
+      // Sections
+      'ytm-reel-shelf-renderer',
+      'ytd-reel-shelf-renderer',
+      'ytm-item-section-renderer:has(.big-shorts-singleton)',
+      'ytd-rich-section-renderer:has(ytd-rich-shelf-renderer)',
+
+      // Videos
+      'ytd-video-renderer:has(a#thumbnail[href*="shorts" i])',
+  ];
 
   function removeElement(el) {
     el.remove()
