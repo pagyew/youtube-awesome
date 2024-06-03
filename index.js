b6d4bfca3ff823e03fbcea41a50f2d2d52b02932
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-06-03 19:59:26 +0600

    fix: :bug: returns old match pattern, because the new one is buggy

--- index.js
+++ index.js
@@ -7,7 +7,8 @@
 // @license      MIT
 // @homepageURL  https://github.com/pagyew/youtube-awesome
 // @supportURL   https://github.com/pagyew/youtube-awesome
-// @match        https://*.youtube.com/*
+// @match        https://www.youtube.com/*
+// @match        https://m.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @run-at       document-body
 // @grant        GM_getValue
