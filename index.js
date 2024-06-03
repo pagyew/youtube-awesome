b2168fec9447d714fb908e69d4a502ea7b3f7318
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-06-03 18:25:40 +0600

    feat: :sparkles: create menus; adds rules for mixes and watched videos

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         YouTube Shortsless
+// @name         YouTube Awesome
 // @namespace    https://github.com/pagyew
 // @version      0.2.1
-// @description  Removes everything related to shorts on YouTube
+// @description  Get the best YouTube experience
 // @author       Vladislav Tsepilov (https://github.com/pagyew)
 // @license      MIT
 // @homepageURL  https://github.com/pagyew/youtube-shortsless
@@ -11,34 +11,128 @@
 // @match        https://m.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @run-at       document-body
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
-  const selectors = [
-      // Menus
-      'ytm-pivot-bar-item-renderer:has(.pivot-shorts)',
-      'ytd-mini-guide-entry-renderer:has(a#endpoint[title="shorts" i])',
-      'ytd-guide-entry-renderer:has(a#endpoint[title="shorts" i])',
+  const shortsSelectors = [
+    // Menus
+    'ytm-pivot-bar-item-renderer:has(.pivot-shorts)',
+    'ytd-mini-guide-entry-renderer:has(a#endpoint[title*="shorts" i])',
+    'ytd-guide-entry-renderer:has(a#endpoint[title*="shorts" i])',
 
-      // Tabs
-      'yt-chip-cloud-chip-renderer:has(yt-formatted-string[title*="shorts" i])',
-      'yt-tab-shape[tab-title="shorts" i]',
+    // Tabs
+    'yt-chip-cloud-chip-renderer:has(yt-formatted-string[title*="shorts" i])',
+    'yt-tab-shape[tab-title*="shorts" i]',
 
-      // Sections
-      'ytm-reel-shelf-renderer',
-      'ytd-reel-shelf-renderer',
-      'ytm-item-section-renderer:has(.big-shorts-singleton)',
-      'ytd-rich-section-renderer:has(ytd-rich-shelf-renderer)',
+    // Sections
+    'ytm-reel-shelf-renderer',
+    'ytd-reel-shelf-renderer',
+    'ytm-item-section-renderer:has(.big-shorts-singleton)',
+    'ytd-rich-section-renderer:has(ytd-rich-shelf-renderer)',
 
-      // Videos
-      'ytd-video-renderer:has(a#thumbnail[href*="shorts" i])',
+    // Videos
+    'ytd-video-renderer:has(a#thumbnail[href*="shorts" i])',
   ];
 
+  const mixSelectors = [
+    // Tabs
+    'yt-chip-cloud-chip-renderer:has(yt-formatted-string[title*=mixes i])',
+
+    // Videos
+    'ytd-rich-item-renderer:has(ytd-playlist-thumbnail):has(a[title^="Mix - "])',
+  ];
+
+  const watchedSelectors = [
+    //Videos
+    'ytd-rich-item-renderer:has(#progress[style="width: 100%;"])',
+  ];
+
+  const options = [
+    {
+      id: 'shorts',
+      title: 'Shorts',
+      defaultValue: true,
+      selectors: shortsSelectors,
+    },
+    {
+      id: 'mix',
+      title: 'Mixes',
+      defaultValue: true,
+      selectors: mixSelectors,
+    },
+    {
+      id: 'watched',
+      title: 'Watched',
+      defaultValue: true,
+      selectors: watchedSelectors,
+    },
+  ];
+
+  function register(option) {
+    const { id, ref, title } = option;
+    const command = `--- ${ref.value ? '✅' : '❌'} ${title}`;
+
+    GM_registerMenuCommand(
+      command,
+      () => {
+        ref.value = !ref.value;
+        setTimeout(update);
+      },
+      {
+        id,
+        autoClose: false,
+      },
+    );
+  };
+
+  function unregister(option) {
+    GM_unregisterMenuCommand(option.id);
+  };
+
+  function useOption(option) {
+    const { id, defaultValue } = option;
+
+    if (typeof GM_getValue === 'undefined') {
+      return { value: defaultValue };
+    };
+
+    const ref = {
+      get value() {
+        return GM_getValue(id, defaultValue);
+      },
+      set value(v) {
+        GM_setValue(id, v);
+      },
+    };
+
+    return { ...option, ref };
+  };
+
   const style = document.createElement('style');
-
-  style.innerHTML = `${selectors.join(',')}{ display: none !important; }`;
-
   document.body.appendChild(style);
+  
+  const usedOptions = options.map(useOption);
+  GM_registerMenuCommand('Hide:', () => { }, { autoClose: false });
+
+  function update() {
+    usedOptions.forEach(unregister);
+    usedOptions.forEach(register);
+
+    const selectors = usedOptions
+      .filter(({ ref }) => ref.value)
+      .map(({ selectors }) => selectors)
+      .flat();
+
+    if (selectors.length > 0) {
+      style.innerHTML = `${selectors.join(',')}{ display: none !important; }`;
+    };
+  };
+
+  update();
 })();
