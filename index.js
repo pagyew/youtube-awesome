57716138d10c3260e7e99b48b3d93c8b92618e1a
Vladislav Tsepilov <fickleprogger@gmail.com>
2025-06-14 09:02:45 +0300

    chore: release v1.0.9

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Awesome
 // @namespace    https://github.com/pagyew
-// @version      1.0.8
+// @version      1.0.9
 // @description  Get the best YouTube experience
 // @author       Vladislav Tsepilov (https://github.com/pagyew)
 // @license      MIT
